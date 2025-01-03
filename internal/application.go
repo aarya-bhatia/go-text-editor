06965c1d29582ec10c9cd682ccebd356a4c9b61d
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2025-01-02 21:02:27 -0800

    add application open/close/next/prev file api

--- internal/application.go
+++ internal/application.go
@@ -1,32 +1,132 @@
 package internal
 
+import "log"
+
 const NORMAL_MODE int = 0
 const INSERT_MODE int = 1
 const COMMAND_MODE int = 2
 
 type Application struct {
-	QuitSignal bool
-	Files      []*File
-  CurrentFile *File
-	StatusLine string
-	Mode       int
+	QuitSignal  bool
+	Files       []*File
+	CurrentFile *File
+	StatusLine  string
+	Mode        int
 }
 
+// Returns new application instance
 func NewApplication() *Application {
 	var this *Application = new(Application)
 	this.QuitSignal = false
 	this.Files = make([]*File, 0)
-  this.CurrentFile = nil
+	this.CurrentFile = nil
 	this.StatusLine = ""
-  this.Mode = NORMAL_MODE
+	this.Mode = NORMAL_MODE
 	return this
 }
 
-func (this *Application) OpenFile(filename string) error {
+// Create new file and make it current
+func (this *Application) AddFile(filename string) error {
 	var file *File = NewFile(filename)
 	this.Files = append(this.Files, file)
-	
+	this.CurrentFile = file
+  log.Println("new file added: ", filename)
 
 	return nil
 }
 
+// Find file in buffer list or create it and make it current
+func (this *Application) OpenFile(filename string) error {
+	for _, file := range this.Files {
+		if file.Name == filename {
+			this.CurrentFile = file
+			return nil
+		}
+	}
+
+	return this.AddFile(filename)
+}
+
+// Close current file and replace it with the next file in buffer list.
+func (this *Application) CloseFile() {
+	if this.CurrentFile == nil || len(this.Files) == 0 {
+    log.Println("buffer list is empty")
+		return
+	}
+
+	if len(this.Files) == 1 {
+		this.CurrentFile = nil
+		this.Files = make([]*File, 0)
+		return
+	}
+
+	for i, file := range this.Files {
+		if file == this.CurrentFile {
+
+			if i+1 < len(this.Files) {
+				this.CurrentFile = this.Files[i+1]
+			} else {
+				this.CurrentFile = this.Files[0]
+			}
+
+			newFiles := make([]*File, 0)
+			newFiles = append(newFiles, this.Files[:i]...)
+			newFiles = append(newFiles, this.Files[i+1:]...)
+
+      this.Files = newFiles
+			return
+		}
+	}
+}
+
+// Open the next file in buffer list
+func (this *Application) OpenNextFile() {
+	if this.CurrentFile == nil && len(this.Files) == 0 {
+		return
+	}
+
+	if this.CurrentFile == nil {
+		this.CurrentFile = this.Files[0]
+		return
+	}
+
+	if this.CurrentFile == this.Files[len(this.Files)-1] {
+		this.CurrentFile = this.Files[0]
+		return
+	}
+
+	for i, file := range this.Files[:len(this.Files)-1] {
+		if file == this.CurrentFile {
+			this.CurrentFile = this.Files[i+1]
+			return
+		}
+	}
+
+  log.Println("No next file")
+}
+
+// Open the previous file in buffer list
+func (this *Application) OpenPrevFile() {
+	if this.CurrentFile == nil && len(this.Files) == 0 {
+		return
+	}
+
+	if this.CurrentFile == nil {
+		this.CurrentFile = this.Files[len(this.Files)-1]
+		return
+	}
+
+	if this.CurrentFile == this.Files[0] {
+		this.CurrentFile = this.Files[len(this.Files)-1]
+		return
+	}
+
+	for i, file := range this.Files[1:] {
+		if file == this.CurrentFile {
+			this.CurrentFile = this.Files[(i+1)-1] // offset i because it is relative to 1-indexed array
+			return
+		}
+	}
+
+  log.Println("No prev file")
+}
---
+++ internal/application_test.go
@@ -0,0 +1,151 @@
+package internal
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApplication_NoFiles(t *testing.T) {
+	var app *Application = NewApplication()
+	assert.True(t, app != nil)
+
+  // Verify the app is initialized with correct values and there are no files
+	assert.True(t, app.CurrentFile == nil)
+	assert.True(t, len(app.Files) == 0)
+	assert.True(t, app.QuitSignal == false)
+	assert.True(t, app.Mode == NORMAL_MODE)
+	assert.True(t, app.StatusLine == "")
+
+  // Verify app can handle edge cases
+  app.OpenPrevFile()
+	assert.True(t, app.CurrentFile == nil)
+  app.OpenNextFile()
+	assert.True(t, app.CurrentFile == nil)
+  app.CloseFile()
+	assert.True(t, app.CurrentFile == nil)
+}
+
+func TestApplication_SingleFile(t *testing.T) {
+	f, _ := os.CreateTemp("", TEMP_FILE)
+  defer f.Close()
+  defer os.Remove(f.Name())
+
+	var app *Application = NewApplication()
+
+  // Verify it can open one file
+  app.OpenFile(f.Name())
+	assert.True(t, app.CurrentFile != nil)
+	assert.True(t, len(app.Files) == 1)
+	assert.True(t, app.CurrentFile.Name == f.Name())
+  file := app.CurrentFile
+
+  // Verify it does not create new buffer if file exists
+  app.OpenFile(f.Name())
+	assert.True(t, app.CurrentFile == file)
+	assert.True(t, len(app.Files) == 1)
+	assert.True(t, app.CurrentFile.Name == f.Name())
+
+  // Verify it does not change current file when moving back and forth
+  app.OpenNextFile()
+	assert.True(t, app.CurrentFile == file)
+  app.OpenPrevFile()
+	assert.True(t, app.CurrentFile == file)
+  app.CloseFile()
+
+  // Verify it closes the file
+	assert.True(t, app.CurrentFile == nil)
+	assert.True(t, len(app.Files) == 0)
+
+}
+
+func TestApplication_MultipleFiles(t *testing.T) {
+	f1, _ := os.CreateTemp("", TEMP_FILE+".0")
+	f2, _ := os.CreateTemp("", TEMP_FILE+".1")
+	f3, _ := os.CreateTemp("", TEMP_FILE+".2")
+
+	defer f1.Close()
+	defer f2.Close()
+	defer f3.Close()
+
+  defer os.Remove(f1.Name())
+  defer os.Remove(f2.Name())
+  defer os.Remove(f3.Name())
+
+	var app *Application = NewApplication()
+
+  // Verify it opens first file
+	app.OpenFile(f1.Name())
+	assert.True(t, app.CurrentFile != nil)
+	assert.True(t, len(app.Files) == 1)
+	assert.True(t, app.CurrentFile.Name == f1.Name())
+	f1_file := app.CurrentFile
+
+  // Verify it opens second file
+	app.OpenFile(f1.Name())
+	app.OpenFile(f2.Name())
+	assert.True(t, app.CurrentFile != nil)
+	assert.True(t, len(app.Files) == 2)
+	assert.True(t, app.CurrentFile.Name == f2.Name())
+	f2_file := app.CurrentFile
+
+  // Verify it opens third file
+	app.OpenFile(f3.Name())
+	assert.True(t, app.CurrentFile != nil)
+	assert.True(t, len(app.Files) == 3)
+	assert.True(t, app.CurrentFile.Name == f3.Name())
+	f3_file := app.CurrentFile
+
+  // Verify it opens the first buffer when opening the first file
+	app.OpenFile(f1.Name())
+	assert.True(t, app.CurrentFile != nil)
+	assert.True(t, len(app.Files) == 3)
+	assert.True(t, app.CurrentFile.Name == f1.Name())
+	assert.True(t, app.CurrentFile == f1_file)
+
+  // Verify it opens the second buffer when opening the second file
+	app.OpenFile(f2.Name())
+	assert.True(t, app.CurrentFile == f2_file)
+	assert.True(t, len(app.Files) == 3)
+
+  // Verify it opens the third buffer when opening the third file
+	app.OpenFile(f3.Name())
+	assert.True(t, app.CurrentFile == f3_file)
+	assert.True(t, len(app.Files) == 3)
+
+  log.Printf("f1: %s, f2: %s, f3: %s", f1.Name(), f2.Name(), f3.Name())
+
+  // Verify it opens the files in the sequence they were added - 1 - 2 - 3 
+  // and it should circle back to the start
+	app.OpenNextFile()
+	assert.True(t, app.CurrentFile == f1_file)
+	app.OpenNextFile()
+	assert.True(t, app.CurrentFile == f2_file)
+	app.OpenNextFile()
+	assert.True(t, app.CurrentFile == f3_file)
+	app.OpenPrevFile()
+  log.Println("Current file: ", app.CurrentFile.Name)
+	assert.True(t, app.CurrentFile == f2_file)
+	app.OpenPrevFile()
+	assert.True(t, app.CurrentFile == f1_file)
+	app.OpenPrevFile()
+	assert.True(t, app.CurrentFile == f3_file)
+
+  // Verify it closes the third file and replaces current file with the next one, which is f1
+	app.CloseFile()
+	assert.True(t, len(app.Files) == 2)
+	assert.True(t, app.CurrentFile == f1_file)
+
+  // Verify it closes the first file and replaces current file with f2
+	app.CloseFile()
+	assert.True(t, len(app.Files) == 1)
+	assert.True(t, app.CurrentFile == f2_file)
+
+  // Verify all files are closed and current file is nil
+	app.CloseFile()
+	assert.True(t, len(app.Files) == 0)
+	assert.True(t, app.CurrentFile == nil)
+}
+
